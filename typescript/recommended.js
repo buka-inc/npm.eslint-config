f5741871be82766ffcb72bd2ee777f3c3d75ed03
val.istar.guo <val.istar.guo@gmail.com>
2023-07-29 22:50:12 +0800

    feat: set no-redundant-type-constituents to warn

--- typescript/recommended.js
+++ typescript/recommended.js
@@ -10,6 +10,7 @@ module.exports = {
     "@typescript-eslint/no-unsafe-member-access": "off",
     "@typescript-eslint/no-unsafe-assignment": "off",
     "@typescript-eslint/no-unsafe-argument": "off",
+    '@typescript-eslint/no-redundant-type-constituents': 'warn',
     "new-cap": [
       "error",
       {
