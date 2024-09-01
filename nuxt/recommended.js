a6554e6bf20d311f1463609fe05e39740c405ca8
val.istar.guo <val.istar.guo@gmail.com>
2024-09-01 15:08:30 +0800

    perf: set ban-ts-comment to warn for nuxt

--- nuxt/recommended.js
+++ nuxt/recommended.js
@@ -3,21 +3,22 @@ module.exports = [
     languageOptions: {
       parserOptions: {
         project: true,
-      }
+      },
     },
 
     rules: {
       '@typescript-eslint/consistent-type-imports': 'off',
       'vue/no-multiple-template-root': 'off',
       '@typescript-eslint/no-explicit-any': 'warn',
-      "@typescript-eslint/explicit-function-return-type": [
-        "error",
+      '@typescript-eslint/ban-ts-comment': 'warn',
+      '@typescript-eslint/explicit-function-return-type': [
+        'error',
         {
-          "allowHigherOrderFunctions": true,
-          "allowExpressions": true,
-          "allowTypedFunctionExpressions": true
-        }
+          allowHigherOrderFunctions: true,
+          allowExpressions: true,
+          allowTypedFunctionExpressions: true,
+        },
       ],
-    }
-  }
+    },
+  },
 ]
--- typescript/recommended.js
+++ typescript/recommended.js
@@ -6,12 +6,12 @@ const typescriptEslint = require('@typescript-eslint/eslint-plugin')
 
 
 module.exports = [
-  ...js.recommended.map(config => ({
+  ...js.recommended.map((config) => ({
     ...config,
     files: ['**/*.js', '**/*.ts'],
   })),
 
-  ...tseslint.configs.recommended.map(config => ({
+  ...tseslint.configs.recommended.map((config) => ({
     ...config,
     files: ['**/*.ts'],
   })),
@@ -29,39 +29,39 @@ module.exports = [
     plugins: {
       '@typescript-eslint': typescriptEslint,
     },
-    "rules": {
-      "@typescript-eslint/no-unsafe-call": "off",
-      "@typescript-eslint/no-unsafe-member-access": "off",
-      "@typescript-eslint/no-unsafe-assignment": "off",
-      "@typescript-eslint/no-unsafe-argument": "off",
-      "@typescript-eslint/no-redundant-type-constituents": "warn",
-      "new-cap": [
-        "error",
+    rules: {
+      '@typescript-eslint/no-unsafe-call': 'off',
+      '@typescript-eslint/no-unsafe-member-access': 'off',
+      '@typescript-eslint/no-unsafe-assignment': 'off',
+      '@typescript-eslint/no-unsafe-argument': 'off',
+      '@typescript-eslint/no-redundant-type-constituents': 'warn',
+      'new-cap': [
+        'error',
         {
-          "capIsNew": false
-        }
+          capIsNew: false,
+        },
       ],
-      "@typescript-eslint/no-floating-promises": "error",
-      "@typescript-eslint/explicit-function-return-type": [
-        "error",
+      '@typescript-eslint/no-floating-promises': 'error',
+      '@typescript-eslint/explicit-function-return-type': [
+        'error',
         {
-          "allowHigherOrderFunctions": true,
-          "allowExpressions": true,
-          "allowTypedFunctionExpressions": true
-        }
+          allowHigherOrderFunctions: true,
+          allowExpressions: true,
+          allowTypedFunctionExpressions: true,
+        },
       ],
-      "no-unused-vars": "off",
-      "@typescript-eslint/no-unused-vars": ["error", {
-        caughtErrors: "none",
+      'no-unused-vars': 'off',
+      '@typescript-eslint/no-unused-vars': ['error', {
+        caughtErrors: 'none',
       }],
-      "@typescript-eslint/ban-ts-comment": "warn",
-      "@typescript-eslint/restrict-template-expressions": ["error", {
+      '@typescript-eslint/ban-ts-comment': 'warn',
+      '@typescript-eslint/restrict-template-expressions': ['error', {
         allowNumber: true,
       }],
-      "no-dupe-class-members": "off",
-      "@typescript-eslint/no-dupe-class-members": "error",
-      "no-redeclare": "off",
-      "@typescript-eslint/no-redeclare": "error",
-    }
-  }
+      'no-dupe-class-members': 'off',
+      '@typescript-eslint/no-dupe-class-members': 'error',
+      'no-redeclare': 'off',
+      '@typescript-eslint/no-redeclare': 'error',
+    },
+  },
 ]
