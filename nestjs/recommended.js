39f52d68556e170569398f273cef53b0188733ca
Val.istar.Guo <val.istar.guo@gmail.com>
2025-11-15 22:40:24 +0800

    fix: off @typescript-eslint/no-unsafe-enum-comparison in nestjs

--- nestjs/recommended.js
+++ nestjs/recommended.js
@@ -5,6 +5,11 @@ import typescript_recommended from '../typescript/recommended.js'
 export default [
   ...typescript_recommended,
   {
+    rules: {
+      '@typescript-eslint/no-unsafe-enum-comparison': 'off',
+    },
+  },
+  {
     languageOptions: {
       globals: {
         ...globals.node,
