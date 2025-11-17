ad0ac04752d93c659829650a52788b52618a5440
Val.istar.Guo <val.istar.guo@gmail.com>
2025-11-18 00:43:09 +0800

    fix: off @typescript-eslint/require-await in nestjs

--- nestjs/recommended.js
+++ nestjs/recommended.js
@@ -6,7 +6,12 @@ export default [
   ...typescript_recommended,
   {
     rules: {
+      // MikroORM 中需要频繁使用 `& Opt`，开启此规则会导致无法断言。
       '@typescript-eslint/no-unsafe-enum-comparison': 'off',
+
+      // NestJS Services 通常是异步的，即使当前没有使用 await，未来可能会添加异步操作。
+      // 这样做可以避免调用添加异步操作时，需要修改整个调用链的问题。
+      '@typescript-eslint/require-await': 'off',
     },
   },
   {
