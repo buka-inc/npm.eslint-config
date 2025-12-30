7aab616aa25fb3567720e006e8fc7b9c3cd9ad15
Val.istar.Guo <val.istar.guo@gmail.com>
2025-12-30 21:59:50 +0800

    fix: off @typescript-eslint/no-empty-object-type in nestjs

--- nestjs/recommended.js
+++ nestjs/recommended.js
@@ -12,6 +12,9 @@ export default [
       // NestJS Services 通常是异步的，即使当前没有使用 await，未来可能会添加异步操作。
       // 这样做可以避免调用添加异步操作时，需要修改整个调用链的问题。
       '@typescript-eslint/require-await': 'off',
+
+      // NestJS 经常需要保留空对象类型以便于扩展。
+      '@typescript-eslint/no-empty-object-type': 'off',
     },
   },
   {
